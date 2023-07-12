602f3d3cec7a91e68dc6b8ad3e2242328ef599eb
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-07-12 21:47:28 +0100

    Added cleanData & getBasePath helper fns

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -6,6 +6,8 @@ import RainChart from "@/components/RainChart";
 import StatCard from "@/components/StatCard";
 import TempChart from "@/components/TempChart";
 import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+import cleanData from "@/lib/cleanData";
+import getBasePath from "@/lib/getBasePath";
 
 export const revalidate = 60;
 
@@ -32,6 +34,21 @@ const WeatherPage = async ({ params: { city, lat, long } }: Props) => {
 
   const results: Root = data.myQuery;
 
+  const dataToSend = cleanData(results, city);
+
+  const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      weatherData: dataToSend,
+    }),
+  });
+
+  const GPTdata = await res.json();
+  const { content } = GPTdata;
+
   return (
     <div className="flex flex-col min-h-screen md:flex-row">
       <InformationPanel city={city} long={long} lat={lat} results={results} />
@@ -47,7 +64,7 @@ const WeatherPage = async ({ params: { city, lat, long } }: Props) => {
           </div>
 
           <div className="m-2 mb-10">
-            <CalloutCard message="This is where GPT summary will go" />
+            <CalloutCard message={content} />
           </div>
           <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
             <StatCard
